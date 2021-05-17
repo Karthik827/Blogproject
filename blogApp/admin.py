71e0e863bcf04e93ee53d4d55b0d8c889b986bff
karthik827 <karthikponnam78@gmail.com>
2021-05-17 10:20:16 +0530

    models created

--- blogApp/admin.py
+++ blogApp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from blogApp.models import Post
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display=['title','author','body','created','publish','updated','status']
+    prepopulated_fields={'slug':('title',)}
+    list_filter= ('title','author','created','publish')
+    search_fields=('title','body',)
+    raw_id_fields=('author',)
+    date_hierarchy='publish'
+    ordering=['status','publish']
+
+
+
+admin.site.register(Post,PostAdmin)
